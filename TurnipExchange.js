84aedcfced0dae88ce98736a0177b80f41d043c3
DESKTOP-F9I26CG\JavaJ <jonathan.henderson24@yahoo.com>
2021-04-18 19:50:08 -0400

    Bug Fixes

--- TurnipExchange.js
+++ TurnipExchange.js
@@ -25,9 +25,9 @@ function grabBestIslands(islands)
     var bestIslands = []
 
     islands.forEach(island => {
-        if(island.turnipPrice >= 400 && island.category === "turnips" && island.patreonOnly === 0)
+        if(island.turnipPrice >= 100 && island.category === "turnips" && island.patreonOnly === 0)
         {
-            if(island.queued.startsWith("0")) bestIslands.push(island)
+            if(parseInt(island.queued.charAt(0)) < 5) bestIslands.push(island)
         }
     });
 
@@ -45,20 +45,20 @@ async function grabBestTurnipPrice(bestIslands)
     joinLobby(bestIsland)
 }
 
-
 async function joinLobby(bestIsland)
 {
-    const browser = await puppeteer.launch({headless: false})
+    const browser = await puppeteer.launch({headless: false, defaultViewport: null, args:['--start-maximized']})
     const page = await browser.newPage()
 
     await page.goto(`https://turnip.exchange/island/${bestIsland.turnipCode.toString()}`, { waitUntil: 'networkidle2'})
     await page.waitFor(1000)
-    await page.click('div[class="bg-info text-info-foreground flex flex-col justify-center items-start p-2 rounded my-4 w-11/12"]')
+    await page.click('div[class="flex flex-col items-start justify-center w-11/12 p-2 my-4 rounded bg-info text-info-foreground"]')
     await page.waitFor(1000)
-    await page.click('button[class="bg-primary hover:bg-primary-600 text-background p-2 rounded-lg "]')
+    await page.click('button[class="p-2 rounded-lg bg-primary hover:bg-primary-600 text-background "]')
     await page.waitFor(3000)
-    await page.type('input[class="border-b-2 border-secondary-200 focus:border-secondary-300 outline-none flex-1"]', 'rachel', {delay:25})
-    await page.waitFor(1000)
-    const joinButton = await page.$$('[class="bg-primary hover:bg-primary-600 text-background p-2 rounded-lg"]')
+    await page.type('input[class="flex-1 border-b-2 outline-none border-secondary-200 focus:border-secondary-300"]', 'rachel', {delay:50})
+    await page.waitFor(2000)
+    const joinButton = await page.$$('[class="p-2 rounded-lg bg-primary hover:bg-primary-600 text-background"]')
+    await page.waitFor(2000)
     await joinButton[1].click()
 }
